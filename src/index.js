71d331bce2e0a455765b2d823a3b6f1cd7de7d6c
Edson Gomes <112134265+biduedson@users.noreply.github.com>
2023-06-27 20:32:44 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,6 +17,4 @@ const storage = multer.diskStorage({
 const upload = multer({ storage })
 module.exports = upload
 
-app.listen(8080, () => {
-    console.log('Servidor rodando!')
-})
+app.listen(8080)
