e72f35200955b751434c3334a9ac4e7560f9712c
biduedson <112134265+biduedson@users.noreply.github.com>
2023-07-11 23:08:15 -0300

    usando authentication na getroutes

--- src/routes/getRoutes.js
+++ src/routes/getRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { hamburguerList, bebidasList, combosList } = require("../controllers/getController.js")
 const getRoutes = express.Router();
+const userAuthentication = require('../middlewares/authentication.js')
 
+getRoutes.use(userAuthentication)
 getRoutes.get("/hamburguers", hamburguerList)
 getRoutes.get("/bebidas", bebidasList)
 getRoutes.get("/combos", combosList)
